1af466e2e41003915d118ae4d4ce3de841081c7c
Minh Nguyễn <mxn@1ec5.org>
2017-12-07 19:02:45 -0800

    Documented each step in the transformation

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ function normalizeSpeed(speed) {
  *      lane.
  * - viaNode {Number} The ID of the node representing the end of the turn lane.
  * - line {LineString} The turn lane's geometry.
- * - turn {String} The allowed maneuver as "reverse", "left", or "right".
+ * - turn {String} The allowed turn as "reverse", "left", or "right".
  * - lanes {Number} The number of lanes that can be used for the maneuver.
  * - protected {Boolean} True if the maneuver has at least one dedicated lane
  *      subject to a lane change restriction.
@@ -113,15 +113,25 @@ function normalizeSpeed(speed) {
  * @returns {Array<Object>} Turn maneuvers allowed by the way.
  */
 function getManeuvers(way, progression) {
+    // Get turn lane indications.
     let laneCount = getLaneCount(way, progression);
     let turnTags = getTagsForProgression("turn", way, progression, laneCount);
     if (!turnTags) {
         return [];
     }
+    
+    // Convert the turn lane indications into a structured format: an array, one
+    // entry per lane, each entry containing an inner array of turn indication
+    // strings.
     turnTags = turnTags.split("|").map(tag => tag.split(";"));
     
+    // Get lane change restrictions.
     let changeTags = getTagsForProgression("change", way, progression, laneCount);
     if (changeTags) {
+        // Convert the lane change restrictions into a structured format: an
+        // array, one entry per lane, each entry an array of two Booleans
+        // indicating whether a change is allowed to the left or the right of
+        // that lane.
         changeTags = changeTags.split("|").map((tag, idx) => {
             switch (tag) {
                 case "yes":
@@ -135,7 +145,8 @@ function getManeuvers(way, progression) {
                 case "only_left":
                     return [true, false];
                 default:
-                    console.assert(false, "Way %s, #%s lane has unrecognized change tag %s", way.id, idx + 1, tag);
+                    console.assert(false, "Way %s, #%s lane has unrecognized change tag %s",
+                                   way.id, idx + 1, tag);
             }
         });
         
@@ -147,13 +158,19 @@ function getManeuvers(way, progression) {
         _.last(changeTags)[1] = undefined;
         
         if (turnTags.length !== changeTags.length) {
-            console.warn("Way %s has %s turn lanes but %s lanes in change:lanes", way.id, turnTags.length, changeTags.length);
+            console.warn("Way %s has %s turn lanes but %s lanes in change:lanes",
+                         way.id, turnTags.length, changeTags.length);
             return [];
         }
     }
     
+    // Model each lane based on its turn indication and lane change
+    // restrictions.
     let lanes = _.zip(turnTags, changeTags).map(pair => _.zipObject(["turn", "change"], pair));
     
+    // Classify the lanes by their turn lane indications. For the purposes of
+    // this analysis, slight turns (such as on exit lanes) are equivalent to
+    // full turns, and merge indications are irrelevant.
     let turns = {
         none: lanes.filter(lane => !lane.turn.length || lane.turn[0] === "none"),
         reverse: lanes.filter(lane => lane.turn.includes("reverse")),
@@ -162,41 +179,67 @@ function getManeuvers(way, progression) {
         right: lanes.filter(lane => lane.turn.includes("right") || lane.turn.includes("slight_right"))
     };
     
+    /**
+     * Returns whether a lane used for the given turn is subject to lane change
+     * restrictions.
+     *
+     * @param turn {String} The allowed turn as "reverse", "left", or "right".
+     * @returns {Boolean} True if a lane used for the turn is subject to lane
+     *  change restrictions, other than the natural restrictions at either side
+     *  of the road.
+     */
     let turnIsProtected = function (turn) {
         return turns[turn].length &&
+            // Does any of the lanes have a solid line to the left?
             _.findIndex(turns[turn], lane => lane.change && !lane.change[0]) !== -1 &&
+            // Does any of the lanes have a solid line to the right?
             _.findIndex(turns[turn], lane => lane.change && !lane.change[1]) !== -1;
     };
     
+    // Determine whether each turn is subject to lane change restrictions.
     let protections = _.mapValues({
-        none: turns.none.length && _.findIndex(turns.none, lane => lane.change && !lane.change[0] && !lane.change[1]) !== -1,
+        // Is any of the unmarked lanes flanked by solid lines?
+        none: turns.none.length &&
+            _.findIndex(turns.none, lane => lane.change && !lane.change[0] && !lane.change[1]) !== -1,
         reverse: turnIsProtected("reverse"),
         left: turnIsProtected("left"),
-        through: turns.through.length && _.findIndex(turns.through, lane => lane.change && !lane.change[0] && !lane.change[1]) !== -1,
+        // Is any of the through lanes flanked by solid lines?
+        through: turns.through.length &&
+            _.findIndex(turns.through, lane => lane.change && !lane.change[0] && !lane.change[1]) !== -1,
         right: turnIsProtected("right")
     }, (protection, turn) => {
         if (!turns[turn].length) {
             return undefined;
         }
+        // If the way only allows this turn and no others, then a lane change
+        // restriction is irrelevant because it doesn't matter which lane the
+        // driver is in.
         let leftmostLane = turns[turn][0];
         let rightmostLane = _.last(turns[turn]);
         if (leftmostLane.change === undefined && rightmostLane.change === undefined) {
             return undefined;
         }
-        if (leftmostLane.change && leftmostLane.change[0] === undefined && rightmostLane.change && rightmostLane.change[1] === undefined) {
+        if (leftmostLane.change && leftmostLane.change[0] === undefined &&
+            rightmostLane.change && rightmostLane.change[1] === undefined) {
             return undefined;
         }
         return protection;
     });
     
+    // Form a line string representing the maneuver's turn lanes.
     let maneuverCoords = turf.getCoords(way.line).concat();
     if (progression < 0) {
         maneuverCoords.reverse();
     }
     let maneuverLine = turf.lineString(maneuverCoords);
     
-    let maxSpeed = getTagsForProgression("maxspeed:advisory", way, progression) || getTagsForProgression("maxspeed", way, progression);
+    // Get the way's maximum speed limit, preferring the advisory speed limit
+    // over the legal speed limit.
+    let maxSpeed = getTagsForProgression("maxspeed:advisory", way, progression) ||
+        getTagsForProgression("maxspeed", way, progression);
     
+    // Return a single maneuver object for each turn type, except for unmarked
+    // turns and for going straight through the intersection.
     return ["reverse", "left", "right"].filter(turn => turns[turn].length).map(turn => ({
         fromWay: way.id,
         progression: progression,
@@ -213,8 +256,9 @@ function getManeuvers(way, progression) {
 /**
  * Merges a maneuver with a connecting maneuver in chronological order.
  *
- * @param maneuver {Object} The maneuver to flatten; its `next` property must be
- *  set to the connecting maneuver.
+ * @param maneuver {Object} The maneuver to flatten. Its `next` property must be
+ *  set to the connecting maneuver, and it must have `fromWays` and
+ *  `progressions` properties.
  * @returns {Object} The same maneuver, flattened to incorporate the information
  *  previously set on the `next` property (which is removed).
  */
@@ -224,18 +268,25 @@ function flattenManeuver(maneuver) {
         return;
     }
     
+    // Recurse down into the next maneuver in case there's a chain of maneuvers.
     flattenManeuver(next);
     
     console.assert(next.isConnection, "Next maneuver %o lacks isConnection property.", next.fromWay);
+    
+    // If a lane change restriction is lifted partway through a turn lane, it
+    // may mean that this tool has too aggressively linked unrelated maneuvers,
+    // or it may signal a tagging error.
     if (maneuver.protected && next.protected === false) {
-        console.warn("Maneuver disallows lane changes at way %s but allows lane changes at way %s", _.last(maneuver.fromWays), next.fromWays[0]);
+        console.warn("Maneuver disallows lane changes at way %s but allows lane changes at way %s",
+                     _.last(maneuver.fromWays), next.fromWays[0]);
     }
     
     // If a maneuver narrows to fewer lanes before the intersection, it may mean
     // that this tool has too aggressively linked unrelated maneuvers, or it may
     // signal a tagging error.
     if (maneuver.lanes > next.lanes) {
-        console.warn("Maneuver drops %s lane(s) from %s to %s", maneuver.lanes - next.lanes, _.last(maneuver.fromWays), next.fromWays[0]);
+        console.warn("Maneuver drops %s lane(s) from %s to %s",
+                     maneuver.lanes - next.lanes, _.last(maneuver.fromWays), next.fromWays[0]);
     }
     
     let length = turf.length(maneuver.line, {
@@ -245,18 +296,27 @@ function flattenManeuver(maneuver) {
         units: "meters"
     });
     
+    // Join the two maneuvers' geometries. The resulting maneuver traverses
+    // multiple ways, not all of which necessarily point in the same direction.
     maneuver.fromWays = maneuver.fromWays.concat(next.fromWays);
     maneuver.progressions = maneuver.progressions.concat(next.progressions);
     maneuver.line = turf.lineString(turf.getCoords(maneuver.line).concat(turf.getCoords(next.line)));
     maneuver.viaNode = next.viaNode;
+    
+    // For a large intersection, the number of lanes for a turn may increase
+    // going toward the intersection.
     maneuver.lanes = Math.max(maneuver.lanes, next.lanes);
     
+    // Detect the beginning of a lane change restriction, if it begins partway
+    // along the turn lane.
     if (!maneuver.protected && next.protected) {
         maneuver.protectionNode = next.fromNode;
     } else {
         maneuver.protectionNode = next.protectionNode;
     }
     
+    // Calculate a average of the speed limits along the turn lane, weighted by
+    // distance.
     if (maneuver.maxSpeed && next.maxSpeed) {
         maneuver.maxSpeed = (length * maneuver.maxSpeed + nextLength * next.maxSpeed) / (length + nextLength);
     } else {
@@ -279,6 +339,7 @@ fs.readFile(input, (err, data) => {
         return;
     }
     
+    // Index the data by IDs.
     let results = JSON.parse(data);
     let elts = results.elements;
     let ways = elts.filter(elt => elt.type === "way");
@@ -286,14 +347,18 @@ fs.readFile(input, (err, data) => {
     let nodes = elts.filter(elt => elt.type === "node");
     let nodesById = _.fromPairs(nodes.map(node => [node.id, node]));
     
+    // Convert individual ways into turn maneuvers.
     let wayIdsByFirstNodeId = {};
     let wayIdsByLastNodeId = {};
     let maneuvers = [];
     ways.forEach(way => {
+        // Form a line string corresopnding to the way.
         let nodes = way.nodes.map(id => nodesById[id]);
         let coords = nodes.map(node => [node.lon, node.lat]);
         way.line = turf.lineString(coords);
         
+        // Index the way by its first and last node IDs, making it easier to
+        // look up connections.
         let firstNodeId = way.nodes[0];
         if (!("firstNodeId" in wayIdsByFirstNodeId)) {
             wayIdsByFirstNodeId[firstNodeId] = [];
@@ -305,12 +370,15 @@ fs.readFile(input, (err, data) => {
         }
         wayIdsByLastNodeId[lastNodeId].push(way.id);
         
+        // A one-lane, one-way link way is most likely a turn channel, which
+        // would occur past the maneuver itself.
         if ((way.tags.turn || (way.tags.lanes === "1")) &&
             (way.tags.oneway === "yes" || way.tags.oneway === "-1") &&
             way.tags.highway.includes("_link")) {
             return;
         }
         
+        // Add one set of maneuvers for each direction of travel along the way.
         let wayManeuvers = [];
         if (!way.tags.oneway || way.tags.oneway === "yes") {
             let forwardManeuvers = getManeuvers(way, 1);
@@ -323,15 +391,36 @@ fs.readFile(input, (err, data) => {
         maneuvers = maneuvers.concat(wayManeuvers);
     });
     
+    // Link up maneuvers that traverse multiple ways.
     maneuvers.forEach(maneuver => {
+        // Two maneuvers are connected if they...
         let connectedManeuvers = maneuvers.filter(otherManeuver =>
+            // Share a node
             otherManeuver.fromNode === maneuver.viaNode &&
+            // Are distinct ways (so not two sides of the same road)
             otherManeuver.fromWay !== maneuver.fromWay &&
+            // Turn the same way (so not a left followed by a right)
             otherManeuver.turn === maneuver.turn
         );
         
+        // Two maneuvers are not connected if...
         let way = waysById[maneuver.fromWay];
+        _.remove(connectedManeuvers, connectedManeuver => {
+            let connectedWay = waysById[connectedManeuver.fromWay];
+            
+            // One is on the main road and the other is on a ramp or turn
+            // channel
+            if ((way.tags.highway !== "service" && connectedWay.tags.highway === "service") ||
+                (!way.tags.highway.includes("_link") && connectedWay.tags.highway.includes("_link"))) {
+                return true;
+            }
+            
+            // A lane change restriction ends after the first maneuver
+            return maneuver.protected && connectedManeuver.protected === false;
+        });
         
+        // Calculate an absolute bearing at the end of the maneuver. The end
+        // coordinate differs based on the maneuver's progression along the way.
         let wayLength = turf.length(way.line, {
             units: "meters"
         });
@@ -350,20 +439,14 @@ fs.readFile(input, (err, data) => {
             units: "meters"
         }));
         
-        _.remove(connectedManeuvers, connectedManeuver => {
-            let connectedWay = waysById[connectedManeuver.fromWay];
-            
-            if ((way.tags.highway !== "service" && connectedWay.tags.highway === "service") ||
-                (!way.tags.highway.includes("_link") && connectedWay.tags.highway.includes("_link"))) {
-                return true;
-            }
-            
-            return maneuver.protected && connectedManeuver.protected === false;
-        });
-        
+        // Calculate a turn angle between the maneuver and each of the connected
+        // maneuvers.
         let bearingDeltas = connectedManeuvers.map(connectedManeuver => {
             let connectedWay = waysById[connectedManeuver.fromWay];
             
+            // Calculate an absolute bearing at the beginning of the connected
+            // maneuver. The starting coordinate differs based on the maneuver's
+            // progression along the way.
             let startOffset;
             let endOffset;
             let connectedWayLength = turf.length(connectedWay.line, {
@@ -382,6 +465,7 @@ fs.readFile(input, (err, data) => {
                 units: "meters"
             }));
             
+            // Find the shorter angular distance between the two bearings.
             let bearingDelta = connectedBearing - bearing;
             if (bearingDelta > 180) {
                 bearingDelta -= 180;
@@ -393,8 +477,14 @@ fs.readFile(input, (err, data) => {
             //}
             return bearingDelta;
         });
+        
+        // Two maneuvers are not connected if they're over 30 degrees apart (in
+        // which case the connected maneuver is probably on a cross street).
         _.remove(connectedManeuvers, (connectedManeuver, idx) => Math.abs(bearingDeltas[idx]) > 30);
         
+        // The maneuver can only be merged with a single maneuver at the same
+        // node. If multiple candidates remain, prefer one with the same road
+        // classification.
         if (connectedManeuvers.length > 1) {
             let sameClassManeuvers = connectedManeuvers.filter(connectedManeuver => {
                 let connectedWay = waysById[connectedManeuver.fromWay];
@@ -405,24 +495,36 @@ fs.readFile(input, (err, data) => {
             }
         }
         
+        // If still multiple candidates remain, prefer one with the same name.
         if (connectedManeuvers.length > 1) {
             let sameNamedManeuvers = connectedManeuvers.filter(connectedManeuver => {
                 let connectedWay = waysById[connectedManeuver.fromWay];
-                return connectedWay.tags.name === way.tags.name;
+                let connectedName = getTagsForProgression("name", connectedWay,
+                                                          connectedManeuver.progression,
+                                                          connectedManeuver.lanes);
+                let name = getTagsForProgression("name", way, maneuver.progression, maneuver.lanes);
+                return connectedName === name;
             });
             if (sameNamedManeuvers.length) {
                 connectedManeuvers = sameNamedManeuvers;
             }
         }
         
-        console.assert(connectedManeuvers.length < 2, "Ambiguous maneuver from %o via one of %o with bearing deltas %s", maneuver, connectedManeuvers, bearingDeltas);
+        // If still multiple candidates remain, there may be a tagging error.
+        console.assert(connectedManeuvers.length < 2,
+                       "Ambiguous maneuver from %o via one of %o with bearing deltas %s",
+                       maneuver, connectedManeuvers, bearingDeltas);
         
+        // Link the maneuver to the only remaining connecting maneuver.
         if (connectedManeuvers.length) {
             maneuver.next = connectedManeuvers[0];
             connectedManeuvers[0].isConnection = true;
         }
     });
     
+    // Prepare the maneuvers to be merged. From this point onward, a maneuver
+    // is assumed to traverse multiple ways, not necessarily in the same
+    // direction along all of them.
     maneuvers = maneuvers.map(maneuver => {
         maneuver.fromWays = [maneuver.fromWay];
         maneuver.progressions = [maneuver.progression];
@@ -431,15 +533,21 @@ fs.readFile(input, (err, data) => {
         return maneuver;
     });
     
+    // Flatten the maneuver array so that each item represents one maneuver
+    // traversing as many ways as necessary.
     maneuvers = maneuvers.filter(maneuver => !maneuver.isConnection);
     maneuvers.forEach(flattenManeuver);
     
+    // Output a tab-delimited representation of each maneuver.
     let writer = output && fs.createWriteStream(output);
     maneuvers.forEach(maneuver => {
         let lastWay = waysById[_.last(maneuver.fromWays)];
         let length = turf.length(maneuver.line, {
             units: "meters"
         });
+        
+        // If only part of the maneuver is subject to lane change restrictions,
+        // determine the length of that part.
         let protectedLength;
         if (maneuver.protectionNode) {
             let protectionNode = nodesById[maneuver.protectionNode];
@@ -451,6 +559,8 @@ fs.readFile(input, (err, data) => {
                 units: "meters"
             });
         }
+        
+        // Output to a file if specified or to standard output otherwise.
         let entry = `${maneuver.fromNode}\t${maneuver.viaNode}\t${maneuver.turn}\t${lastWay.tags.highway}\t${maneuver.lanes}\t${length}\t${protectedLength || ""}\t${maneuver.maxSpeed || ""}`;
         if (writer) {
             writer.write(entry + "\n");
